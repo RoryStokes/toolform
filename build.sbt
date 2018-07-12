918eedd808058d81e4259589576042ce1698adfa
Rory.Stokes <rmstokes@agiledigital.com.au>
2018-07-12 21:34:33 +0100

    Giving up on skuber

--- build.sbt
+++ build.sbt
@@ -29,7 +29,8 @@ libraryDependencies ++= Seq(
   "com.beachape"          %% "enumeratum"            % "1.5.12",
   "org.typelevel"         %% "cats-core"             % "1.0.1",
   "com.spotify"           % "docker-client"          % "8.8.3",
-  "com.goyeau"            %% "kubernetes-client"     % "0.0.5"
+  "com.goyeau"            %% "kubernetes-client"     % "0.0.5",
+  "io.skuber"             %% "skuber"                % "2.0.8"
 )
 
 // For Settings/Task reference, see http://www.scala-sbt.org/release/sxr/sbt/Keys.scala.html
--- src/main/scala/au/com/agiledigital/toolform/command/build/kubernetes/KubernetesBuildEnvironment.scala
+++ src/main/scala/au/com/agiledigital/toolform/command/build/kubernetes/KubernetesBuildEnvironment.scala
@@ -10,112 +10,115 @@ import cats.data.NonEmptyList
 import cats.implicits._
 import com.goyeau.kubernetes.client.{KubeConfig, KubernetesClient, KubernetesException}
 import com.spotify.docker.client.{DockerClient, LogStream}
-import io.k8s.api.core.v1._
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.FiniteDuration
 import java.util.concurrent.TimeUnit.SECONDS
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Scheduler}
+import akka.http.javadsl.model.ws.Message
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.stream.ActorMaterializer
-import io.k8s.api.apps.v1beta2.{Deployment, DeploymentSpec}
-import io.k8s.apimachinery.pkg.apis.meta.v1.{LabelSelector, ObjectMeta}
+import akka.pattern.after
+import akka.stream.scaladsl.Flow
+import io.k8s.apimachinery.pkg.apis.meta.v1.Status
+import skuber.Container.Running
+import skuber.Pod.Phase
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Success, Try}
+import skuber._
+import skuber.json.format._
+import skuber.apps.v1.Deployment
 
 class KubernetesBuildEnvironment extends BuildEnvironment {
 
   val defaultAwait = FiniteDuration(60, SECONDS)
+  val delay        = FiniteDuration(10, SECONDS)
 
-  def withClient[A](inner: KubernetesClient => Future[Either[NonEmptyList[ToolFormError], A]]): Either[NonEmptyList[ToolFormError], A] = {
-    implicit val system       = ActorSystem()
+  implicit var system = ActorSystem()
+
+  def withClient[A](inner: (KubernetesClient, K8SRequestContext) => Future[Either[NonEmptyList[ToolFormError], A]]): Either[NonEmptyList[ToolFormError], A] = {
+    val configFile = new File("/home/rory/.kube/microk8s.config")
+    system = ActorSystem()
     implicit val materializer = ActorMaterializer()
-    val client                = KubernetesClient(KubeConfig(new File("/home/rory/.kube/microk8s.config")))
+    val client                = KubernetesClient(KubeConfig(configFile))
 
-    val fut = for {
-      result <- inner(client)
-    } yield result
-    val result = Try(Await.result(fut, defaultAwait)).toEither.leftMap { e =>
+    K8SConfiguration.parseKubeconfigFile(configFile.toPath).toEither.leftMap { e =>
       e.printStackTrace()
-      NonEmptyList.of(ToolFormError(s"Failed to execute Kubernetes commands: [${e.getMessage}]"))
-    }.joinRight
-    materializer.shutdown()
-    Await.result(system.terminate(), defaultAwait)
-    result
+      NonEmptyList.of(ToolFormError(s"Failed to parse kubernetes config Kubernetes commands: [${configFile.getAbsolutePath}]"))
+    } flatMap { config =>
+      val k8s = k8sInit(config)
+
+      val fut = for {
+        result <- inner(client, k8s)
+      } yield result
+      val result = Try(Await.result(fut, defaultAwait)).toEither.leftMap { e =>
+        e.printStackTrace()
+        NonEmptyList.of(ToolFormError(s"Failed to execute Kubernetes commands: [${e.getMessage}]"))
+      }.joinRight
+      materializer.shutdown()
+      Await.result(system.terminate(), defaultAwait)
+      result
+    }
+
   }
 
-  override def executeInit(builderConfig: BuilderConfig) = withClient { client =>
-    val namespaceDef: Namespace = Namespace(
-      metadata = Some(
-        ObjectMeta(
-          name = Some(builderConfig.namespace)
-        ))
+  def retryUntilSome[T](op: => Future[Option[T]], delay: FiniteDuration, retries: Int): Future[Either[NonEmptyList[ToolFormError], T]] =
+    op flatMap {
+      case None if retries > 0 => after(delay, system.scheduler)(retryUntilSome(op, delay, retries - 1))
+      case Some(value)         => Future.successful(Right(value))
+      case _                   => Future.successful(Left(NonEmptyList.of(ToolFormError(s"Operation timed out after multiple retries"))))
+    }
+
+  def getPod(selector: LabelSelector, client: KubernetesClient, k8s: K8SRequestContext): Future[Option[Pod]] =
+    (k8s listSelected [PodList] selector) map { pods: PodList =>
+      pods.toList.find(_.status.flatMap(_.phase).contains(Phase.Running))
+    }
+
+  def copyToPod(builderConfig: BuilderConfig, pod: Pod, source: Path, destination: String, client: KubernetesClient, k8s: K8SRequestContext
+               ): Future[Either[NonEmptyList[ToolFormError], Unit]] = {
+    val flow = Flow.fromFunction { x: Either[Status, String] =>
+      TextMessage("No idea what this is for")
+    }.mapMaterializedValue(_ => Future.successful("Done"))
+    client.pods.namespace(k8s.namespaceName).exec[String](
+      pod.name,
+      flow,
+      container = Some("tfbuilder"),
+      command = Seq.empty,
+      stdin = false,
+      stdout = true,
+      stderr = true,
+      tty = false
     )
 
-    val deploymentDef = Deployment(
-      metadata = Option(ObjectMeta(name = Option(builderConfig.containerName), namespace = Option(builderConfig.namespace))),
-      spec = Option(
-        DeploymentSpec(
-          template = PodTemplateSpec(
-            metadata = Option(
-              ObjectMeta(
-                labels = Option(Map("app" -> "web", "tier" -> "frontend", "environment" -> "myenv"))
-              )
-            ),
-            spec = Option(
-              PodSpec(
-                containers = Seq(
-                  Container(
-                    name = "tfbuilder",
-                    image = Some(builderConfig.image),
-                    volumeMounts = Option(Seq(VolumeMount(name = "nginx-config", mountPath = "/etc/nginx/conf.d"))),
-                    ports = Option(Seq(ContainerPort(name = Option("http"), containerPort = 8080)))
-                  )
-                ),
-                volumes = Option(
-                  Seq(
-                    Volume(
-                      name = "nginx-config",
-                      configMap = Option(ConfigMapVolumeSource(name = Option("nginx-config")))
-                    )
-                  )
-                )
-              )
-            )
-          )
-        )
+    Future.successful(Right(()))
+  }
+
+  override def executeInit(builderConfig: BuilderConfig) = withClient {
+    case (client, k8s) =>
+      val containerDef = Container(
+        name = "tfbuilder",
+        image = builderConfig.image,
+        command = List("tail", "-f", "/dev/null")
       )
-    )
 
-    println(deploymentDef)
+      val selector = LabelSelector(LabelSelector.IsEqualRequirement("component", builderConfig.containerName))
 
-    for {
-      namespace <- client.namespaces.get(builderConfig.namespace).recoverWith {
-                    case e: KubernetesException if e.statusCode == 404 =>
-                      client.namespaces.create(namespaceDef).map(_ => namespaceDef)
-                  }
-      deployment <- client.deployments.namespace(builderConfig.namespace).create(deploymentDef)
-    } yield Right(())
+      val template = Pod.Template.Spec.named("tfbuilder").addContainer(containerDef).addLabel("component" -> builderConfig.containerName)
 
-//    val hostConfig = HostConfig.builder
-//      .appendBinds(
-//        bindPath(builderConfig.sourceDir, "source"),
-//        bindPath(builderConfig.stagingDir.resolve("test_results"), "test_results"),
-//        bindPath(builderConfig.stagingDir.resolve("artifacts"), "artifacts")
-//      )
-//      .build()
-//
-//    val containerConfig = ContainerConfig.builder
-//      .image(builderConfig.image)
-//      .cmd("tail", "-f", "/dev/null")
-//      .hostConfig(hostConfig)
-//      .build()
-//
-//    val creation    = docker.createContainer(containerConfig, builderConfig.containerName)
-//    val containerId = creation.id
-//
-//    docker.startContainer(containerId)
+      val deploymentDef = Deployment(builderConfig.containerName)
+        .withReplicas(1)
+        .withTemplate(template)
+        .withLabelSelector(selector)
+
+      for {
+        dep <- (k8s getOption [Deployment] builderConfig.containerName) flatMap {
+                case Some(_) => k8s update deploymentDef
+                case None    => k8s create deploymentDef
+              }
+        pod <- retryUntilSome(getPod(selector, client, k8s), delay, 12)
+      } yield pod.map(_ => ())
   }
 
   override def executeScript(script: String, builderConfig: BuilderConfig, optional: Boolean = false): Either[NonEmptyList[ToolFormError], Unit] =
